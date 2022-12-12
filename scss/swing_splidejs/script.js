c4d430e7a830b3df81776d0cc20d7696e4680418
william <101874444+snowman12320@users.noreply.github.com>
2022-12-12 16:10:34 +0800

    first commit

---
+++ scss/swing_splidejs/script.js
@@ -0,0 +1,28 @@
+const splide = new Splide(".splide", {
+  type: "slide", //slide, loop, fade
+  // padding: { y: 10 }, //css values
+  gap: "20px", //1 rem default
+  // rewind: true,
+  speed: 500, //transition in milliseconds
+  fixedWidth: '10vw',
+  width: '100vw',
+  perPage: 5,
+  start: 3,
+  perMove: 2,
+  autoplay: true,
+  interval: 3000,
+  arrows: false,
+  pagination: false,
+  pauseOnHover: true,
+  wheel: true,
+  //
+  // type: "loop",
+  // drag: "free",
+  // focus: "center",
+  // perPage: 3,
+  // autoScroll: {
+  //   speed: -1,
+  // },
+  // 
+});
+splide.mount();
