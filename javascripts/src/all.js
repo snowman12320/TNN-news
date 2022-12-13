e1dd53a79fd7694167de3d99cd98791239263060
william <101874444+snowman12320@users.noreply.github.com>
2022-12-13 18:00:13 +0800

    index ing

--- javascripts/src/all.js
+++ javascripts/src/all.js
@@ -1,1 +1,28 @@
-<script type="module" src="module.js"></script>
+const splide = new Splide(".splide", {
+  type: "slide", //slide, loop, fade
+  // padding: { y: 10 }, //css values
+  gap: "20px", //1 rem default
+  rewind: true,
+  speed: 500, //transition in milliseconds
+  // fixedWidth: "100vw",
+  // width: "100vw",
+  perPage: 1,
+  start: 0,
+  perMove: 1,
+  // autoplay: true,
+  interval: 3000,
+  arrows: true,
+  pagination: true,
+  pauseOnHover: true,
+  wheel: true,
+  //
+  // type: "loop",
+  // drag: "free",
+  // focus: "center",
+  // perPage: 3,
+  // autoScroll: {
+  //   speed: -1,
+  // },
+  //
+});
+splide.mount();
